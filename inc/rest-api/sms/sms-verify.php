8ab95ba98dd1bcfa25514c0c2d5725321f868f83
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 20:46:50 +0530

    Extands rest controller

--- inc/rest-api/sms/sms-verify.php
+++ inc/rest-api/sms/sms-verify.php
@@ -1,36 +1,53 @@
 <?php
 
-class Nts_Sms_Verify extends WP_REST_Controller{
+class Nts_Sms_Verify extends WP_REST_Controller {
 
-	public $name_space = 'sms/v1';
-	public $endpoint = '/verify';
+	//The namespace and version for the REST SERVER
+	var $my_namespace = 'sms/v';
+	var $my_version = '1';
 
-	public function register_route() {
+	public function register_routes() {
+		$namespace = $this->my_namespace . $this->my_version;
+		$base      = 'verify';
 		register_rest_route(
-			$this->name_space,
-			$this->endpoint,
-			[
-				'methods'             => 'GET',
-				'callback'            => [ $this, 'get_items' ],
-				'permission_callback' => [ $this, 'get_items_permissions_check' ],
-			]
+			$namespace,
+			'/' . $base,
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_latest_post' ),
+					'permission_callback' => array( $this, 'get_latest_post_permission' ),
+				),
+				array(
+					'methods'             => WP_REST_Server::CREATABLE,
+					'callback'            => array( $this, 'add_post_to_category' ),
+					'permission_callback' => array( $this, 'add_post_to_category_permission' ),
+				),
+			)
 		);
 	}
 
-	public function get_items( $request ) {
+	// Register our REST Server
+	public function hook_rest_server() {
+		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+	}
+
+	public function get_latest_post_permission() {
+		return true;
+	}
+
+	public function get_latest_post( WP_REST_Request $request ) {
 		return new WP_REST_Response( $request, 200 );
 	}
 
-	public function get_items_permissions_check($request) {
-		return true;
+	public function add_post_to_category_permission() {
+		return $this->get_latest_post_permission();
+	}
+
+	public function add_post_to_category( WP_REST_Request $request ) {
+		return new WP_REST_Response( [ 'message' => 'Created' ], 200 );;
 	}
 }
 
-
-add_action('rest_api_init', [ $this, 'register_route' ] );
-add_action( 'rest_api_init',
-	function() {
-		$latest_posts_controller = new Nts_Sms_Verify();
-		$latest_posts_controller->register_routes();
-	}
-);
+$my_rest_server = new Nts_Sms_Verify();
+$my_rest_server->hook_rest_server();
