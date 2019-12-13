703c4210e8a19d85bfbd06e90d9bbf8950361191
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 22:07:38 +0530

    Send sms verify

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
 	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
 }
 
-require_once __DIR__ . 'config.php';
+require_once __DIR__ . '/config.php';
 require_once __DIR__ . 'vendor/autoload.php';
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client($basic);
