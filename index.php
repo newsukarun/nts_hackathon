018278c5f0d83bdbadb84fe2cab94754eb24054a
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 00:08:36 +0530

    Css changes

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 </head>
 <body>
 <div class="container">
-	<div class="row">
+	<div class="h-100 row align-items-center">
 		<?php
 		if ( ! is_user_logged_in() ) {
 			echo do_shortcode( '[wp_login_form]' );
