eca16f7ef94dfba34bee188727c927e62e85ca15
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 00:21:50 +0530

    Css changes

--- index.php
+++ index.php
@@ -18,6 +18,33 @@
 		html, body {
 			height: 100%;
 		}
+
+		#loginform{
+			padding: 33px;
+			border: 1px solid #ccc;
+			border-radius: 10px;
+			box-shadow: 1px 1px #ccc;
+		}
+
+		#loginform label{
+			width: 100%;
+		}
+
+		#loginform input{
+			display: block;
+			width: 100%;
+			height: calc(1.5em + .75rem + 2px);
+			padding: .375rem .75rem;
+			font-size: 1rem;
+			font-weight: 400;
+			line-height: 1.5;
+			color: #495057;
+			background-color: #fff;
+			background-clip: padding-box;
+			border: 1px solid #ced4da;
+			border-radius: .25rem;
+			transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+		}
 	</style>
 </head>
 <body>
