289e5fbef37616d462979743139b231111826bd8
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 18:56:27 +0530

    User Wp Login forms

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo get_bloginfo( 'title' ); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
 <?php
 if ( ! is_user_logged_in() ) {
 	echo do_shortcode( '[wp_login_form]' );
 
 	return;
 }
+?>
+</body>
+<?php wp_footer(); ?>
+</html>
