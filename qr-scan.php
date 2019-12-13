511a7974658086739a885e6c90a1495be842acb2
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 05:27:36 +0530

    Qr code

--- qr-scan.php
+++ qr-scan.php
@@ -1,8 +1,2 @@
 <?php
 /* Template Name: QR Code Scanner */
-
-use Zxing\QrReader;
-
-require __DIR__ . "/inc/QR-CODE/vendor/autoload.php";
-$qrcode = new QrReader('path/to_image');
-$text = $qrcode->text();
