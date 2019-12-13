9caeaac49916a0ac9693a62d1740cb9fff179f40
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 01:01:41 +0530

    Qr code

--- qr.php
+++ qr.php
@@ -7,6 +7,18 @@ require __DIR__ . '/inc/QR-CODE/vendor/autoload.php';
 
 use Endroid\QrCode\QrCode;
 
-$qrCode = new QrCode( json_encode( ['user_name' => 'Arun'] ) );
+$qrCode = new QrCode(
+	json_encode(
+		[
+			'user_info'    => [
+				wp_get_current_user(),
+			],
+			'date'         => current_datetime(),
+			'employee_id'  => get_field( 'employee_id', get_current_user_id() ),
+			'user_package' => get_field( 'meal_courses', get_current_user_id() ),
+			'user_package' => get_field( 'phone_number', get_current_user_id() ),
+		]
+	)
+);
 header( 'Content-Type: ' . $qrCode->getContentType() );
 echo $qrCode->writeString();
