d6c595e161d8bc23c482c1b077a42d346b86ec06
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 22:09:09 +0530

    Send sms verify

--- functions.php
+++ functions.php
@@ -6,13 +6,12 @@ if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
 	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
 }
 
-require_once __DIR__ . '/config.php';
 require_once __DIR__ . 'vendor/autoload.php';
-$basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
+$basic  = new \Nexmo\Client\Credentials\Basic('9be48f1a', 'R5Jj2YEmCJOvxwEs');
 $client = new \Nexmo\Client($basic);
 try {
 	$message = $client->message()->send([
-		                                    'to' => TO_NUMBER,
+		                                    'to' => 9618583087,
 		                                    'from' => 'Acme Inc',
 		                                    'text' => 'A text message sent using the Nexmo SMS API'
 	                                    ]);
