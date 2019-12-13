415446afdf9a2e22c76638ab42be84f6a3e21056
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 22:06:19 +0530

    Send sms verify

--- functions.php
+++ functions.php
@@ -6,13 +6,22 @@ if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
 	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
 }
 
-require_once "vendor/autoload.php";
-$basic   = new Nexmo\Client\Credentials\Basic( '9be48f1a', 'R5Jj2YEmCJOvxwEs' );
-$client  = new Nexmo\Client( $basic );
-$message = $client->message()->send(
-	[
-		'to'   => '919618583087',
-		'from' => 'Nexmo',
-		'text' => 'Hello from Nexmo',
-	]
-);
+require_once __DIR__ . 'config.php';
+require_once __DIR__ . 'vendor/autoload.php';
+$basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
+$client = new \Nexmo\Client($basic);
+try {
+	$message = $client->message()->send([
+		                                    'to' => TO_NUMBER,
+		                                    'from' => 'Acme Inc',
+		                                    'text' => 'A text message sent using the Nexmo SMS API'
+	                                    ]);
+	$response = $message->getResponseData();
+	if($response['messages'][0]['status'] == 0) {
+		echo "The message was sent successfully\n";
+	} else {
+		echo "The message failed with status: " . $response['messages'][0]['status'] . "\n";
+	}
+} catch (Exception $e) {
+	echo "The message was not sent. Error: " . $e->getMessage() . "\n";
+}
