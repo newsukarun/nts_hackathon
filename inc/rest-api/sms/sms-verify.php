559de461d04097f773fec781164c6329e1db1f6a
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 20:30:52 +0530

    Extands rest controller

--- inc/rest-api/sms/sms-verify.php
+++ inc/rest-api/sms/sms-verify.php
@@ -2,26 +2,35 @@
 
 class Nts_Sms_Verify extends WP_REST_Controller{
 
-	public $name_space = 'sms/v1/';
-	public $endpoint = 'verify';
-	public function __construct() {
-		add_action('rest_api_init', [ $this, 'rest_route' ] );
-	}
+	public $name_space = 'sms/v1';
+	public $endpoint = '/verify';
 
-	public function rest_route() {
+	public function register_route() {
 		register_rest_route(
 			$this->name_space,
 			$this->endpoint,
 			[
-				'methods'  => 'GET',
-				'callback' => [ $this, 'process_verify' ],
+				'methods'             => 'GET',
+				'callback'            => [ $this, 'process_verify' ],
+				'permission_callback' => [ $this, 'get_items_permissions_check' ],
 			]
 		);
 	}
 
 	public function process_verify( WP_REST_Request $request ) {
-		print_r( $request );
+		return new WP_REST_Response( $request, 200 );
+	}
+
+	public function get_items_permissions_check($request) {
+		return true;
 	}
 }
 
-new Nts_Sms_Verify();
+
+add_action('rest_api_init', [ $this, 'register_route' ] );
+add_action( 'rest_api_init',
+	function() {
+		$latest_posts_controller = new Nts_Sms_Verify();
+		$latest_posts_controller->register_routes();
+	}
+);
