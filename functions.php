eb104374841794a81b45e07192d7dcf76a89e709
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 21:56:35 +0530

    Using neximo Api service

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@
 if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
 	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
 }
+
+$basic  = new \Nexmo\Client\Credentials\Basic('9be48f1a', 'R5Jj2YEmCJOvxwEs');
+$client = new \Nexmo\Client($basic);
+$message = $client->message()->send(
+	[
+		'to'   => '919618583087',
+		'from' => 'Nexmo',
+		'text' => 'Hello from Nexmo',
+	]
+);
