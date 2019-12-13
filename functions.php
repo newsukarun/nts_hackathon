9ef0e5bdefd9fd1ad983da90655da9adc7f1b13d
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-13 22:18:56 +0530

    Send sms verify

--- functions.php
+++ functions.php
@@ -1,20 +1,22 @@
 <?php
-/**
- * Rest Api End-point.
- */
-if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
-	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
-}
+///**
+// * Rest Api End-point.
+// */
+//if ( file_exists( __DIR__ . '/inc/rest-api/sms/sms-verify.php' ) ) {
+//	require_once __DIR__ . '/inc/rest-api/sms/sms-verify.php';
+//}
 
 require_once __DIR__ . '/vendor/autoload.php';
 $basic  = new Nexmo\Client\Credentials\Basic('9be48f1a', 'R5Jj2YEmCJOvxwEs');
 $client = new Nexmo\Client($basic);
 try {
-	$message = $client->message()->send([
-		                                    'to' => 9618583087,
-		                                    'from' => 'Acme Inc',
-		                                    'text' => 'A text message sent using the Nexmo SMS API'
-	                                    ]);
+	$message = $client->message()->send(
+		[
+			'to'   => +919618583087,
+			'from' => 'Acme Inc',
+			'text' => 'A text message sent using the Nexmo SMS API',
+		]
+	);
 	$response = $message->getResponseData();
 	if($response['messages'][0]['status'] == 0) {
 		echo "The message was sent successfully\n";
