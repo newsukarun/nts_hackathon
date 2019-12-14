b44c832714c47248917cffe376e0525684aff350
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 06:57:55 +0530

    Qr code

--- qr-scan.php
+++ qr-scan.php
@@ -2,57 +2,32 @@
 /* Template Name: QR Code Scanner */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>Title</title>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/inc/QR-SCANNER/jquery.webcam.js"></script>
+	<title>QR Code Scanner</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 </head>
 <body>
-<div id="webcam"></div>
-<script type="text/javascript">
-jQuery("#webcam").webcam({
 
-	width: 320,
-	height: 240,
-	mode: "callback",
-	swffile: "inc/QR-SCANNER/jscam_canvas_only.swf", // canvas only doesn't implement a jpeg encoder, so the file is much smaller
+    <h1>JQuery HTML5 QR Code Scanner using Instascan JS Example - ItSolutionStuff.com</h1>
 
-	onTick: function(remain) {
+    <video id="preview"></video>
+    <script type="text/javascript">
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      scanner.addListener('scan', function (content) {
+        alert(content);
+      });
+      Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch(function (e) {
+        console.error(e);
+      });
+    </script>
 
-		if (0 == remain) {
-			jQuery("#status").text("Cheese!");
-		} else {
-			jQuery("#status").text(remain + " seconds remaining...");
-		}
-	},
-
-	onSave: function(data) {
-
-		var col = data.split(";");
-    // Work with the picture. Picture-data is encoded as an array of arrays... Not really nice, though =/
-	},
-
-	onCapture: function () {
-		webcam.save();
-
- 	  // Show a flash for example
-	},
-
-	debug: function (type, string) {
-		// Write debug information to console.log() or a div, ...
-	},
-
-	onLoad: function () {
-    // Page load
-		var cams = webcam.getCameraList();
-		for(var i in cams) {
-			jQuery("#cams").append("<li>" + cams[i] + "</li>");
-		}
-	}
-});
-
-</script>
 </body>
 </html>
