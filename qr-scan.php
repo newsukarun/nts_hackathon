df991c9c76530b5f840b0081d604a4de7167e1b7
achaitanyajami <arun.chaitanyajami@news.co.uk>
2019-12-14 07:02:10 +0530

    Qr code

--- qr-scan.php
+++ qr-scan.php
@@ -1,33 +1,97 @@
 <?php
 /* Template Name: QR Code Scanner */
 ?>
-<!DOCTYPE html>
 <html>
-<head>
-	<title>QR Code Scanner</title>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-</head>
-<body>
-
-    <h1>JQuery HTML5 QR Code Scanner using Instascan JS Example - ItSolutionStuff.com</h1>
-
-    <video id="preview"></video>
+    <head>
+        <style type="text/css">
+                .scanner-laser{
+                        position: absolute;
+                        margin: 40px;
+                        height: 30px;
+                        width: 30px;
+                }
+                .laser-leftTop{
+                        top: 0;
+                        left: 0;
+                        border-top: solid red 5px;
+                        border-left: solid red 5px;
+                }
+                .laser-leftBottom{
+                        bottom: 0;
+                        left: 0;
+                        border-bottom: solid red 5px;
+                        border-left: solid red 5px;
+                }
+                .laser-rightTop{
+                        top: 0;
+                        right: 0;
+                        border-top: solid red 5px;
+                        border-right: solid red 5px;
+                }
+                .laser-rightBottom{
+                        bottom: 0;
+                        right: 0;
+                        border-bottom: solid red 5px;
+                        border-right: solid red 5px;
+                }
+        </style>
+    </head>
+    <body>
+        <div style="position: relative;display: inline-block;">
+            <canvas id="qr-canvas" width="320" height="240"></canvas>  // id="qr-canvas" is important!
+            <div class="scanner-laser laser-rightBottom" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-rightTop" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-leftBottom" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-leftTop" style="opacity: 0.5;"></div>
+        </div>
+    </body>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/qrcodelib.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/WebCodeCam.js"></script>
     <script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content) {
-        alert(content);
-      });
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
-        } else {
-          console.error('No cameras found.');
-        }
-      }).catch(function (e) {
-        console.error(e);
-      });
+    //  defalut-settings
+        $('#qr-canvas').WebCodeCam({
+            ReadQRCode: true, // false or true
+            ReadBarecode: true, // false or true
+            width: 320,
+            height: 240,
+            videoSource: {
+                    id: true,      //default Videosource
+                    maxWidth: 640, //max Videosource resolution width
+                    maxHeight: 480 //max Videosource resolution height
+            },
+            flipVertical: false,  // false or true
+            flipHorizontal: false,  // false or true
+            zoom: -1, // if zoom = -1, auto zoom for optimal resolution else int
+            beep: "js/beep.mp3", // string, audio file location
+            autoBrightnessValue: false, // functional when value autoBrightnessValue is int
+            brightness: 0, // int
+            grayScale: false, // false or true
+            contrast: 0, // int
+            threshold: 0, // int
+            sharpness: [], //or matrix, example for sharpness ->  [0, -1, 0, -1, 5, -1, 0, -1, 0]
+            resultFunction: function(resText, lastImageSrc) {
+                        /* resText as decoded code, lastImageSrc as image source
+                        example:
+                        alert(resText);
+                        */
+            },
+            getUserMediaError: function() {
+                        /* callback funtion to getUserMediaError
+                        example:
+                        alert('Sorry, the browser you are using doesn\'t support getUserMedia');
+                        */
+            },
+            cameraError: function(error) {
+                        /* callback funtion to cameraError,
+                        example:
+                        var p, message = 'Error detected with the following parameters:\n';
+                        for (p in error) {
+                                message += p + ': ' + error[p] + '\n';
+                        }
+                        alert(message);
+                        */
+            }
+        });
     </script>
-
-</body>
 </html>
